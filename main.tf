c934a68fdbde4d7589f912f6b155000a360628b8
gorstak17 <loncar.aco@gmail.com>
2025-03-25 09:48:24 +0100

    update version

--- main.tf
+++ main.tf
@@ -146,7 +146,7 @@ resource "aws_lambda_function" "trigger_ecs_task" {
   function_name    = "TriggerOCREcsTask"
   role             = aws_iam_role.lambda_role.arn
   handler          = "lambda_function.lambda_handler"
-  runtime          = "python3.8"
+  runtime          = "python3.11"
   filename         = "lambda_function.zip"
   source_code_hash = filebase64sha256("lambda_function.zip")
   environment {
